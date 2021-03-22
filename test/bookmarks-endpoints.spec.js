f9d44886e428ff00389baf0aa9a0c1f1f840e864
David Tam <davetam88@gmail.com>
2021-03-21 22:06:44 -0400

    final commit bofore heroku deployment

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -9,7 +9,7 @@ describe('Bookmarks Endpoints', function () {
 
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     })
 
     app.set('db', db)
